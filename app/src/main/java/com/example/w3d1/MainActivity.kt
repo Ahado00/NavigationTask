78bff5d9b0d20edec9a18155cb072ab585527790
Ahad <ahadsaad45@gmail.com>
2025-03-16 19:45:34 +0300

    Apply Second Task Requirement

--- app/src/main/java/com/example/w3d1/MainActivity.kt
+++ app/src/main/java/com/example/w3d1/MainActivity.kt
@@ -17,13 +17,16 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.w3d1.ui.theme.W3D1Theme
-
 
 
 class MainActivity : ComponentActivity() {
@@ -33,50 +36,71 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val navController: NavHostController = rememberNavController()
+            var title by remember { mutableStateOf("First Screen") }
+
             W3D1Theme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    topBar = {
-                        TopAppBar(
-                            title = { Text(text = "First Screen") },
-                            navigationIcon = {
-                                IconButton(onClick = { navController.popBackStack() }) {
-                                    Icon(
-                                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                                        contentDescription = "Back"
-                                    )
-                                }
-                            },
-                            actions = {
-                                IconButton(onClick = {}) {
-                                    Icon(
-                                        imageVector = Icons.Filled.MoreVert,
-                                        contentDescription = "More Options"
-                                    )
-                                }
-                            }
-                        )
-                    }
+                    topBar = { DynamicTopAppBar(title, navController) }
                 ) { innerPadding ->
-                    NavigationHost(innerPadding, navController)
+                    NavigationHost(innerPadding, navController, setTitle = { newTitle ->
+                        title = newTitle
+                    })
                 }
             }
         }
     }
-}
 
 
-@Composable
-fun NavigationHost(innerPadding: PaddingValues, navController: NavHostController) {
-    NavHost(
-        navController = navController,
-        startDestination = "firstScreen", // Define a string route
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(innerPadding)
+    @Composable
+    fun NavigationHost(
+        innerPadding: PaddingValues,
+        navController: NavHostController,
+        setTitle: (String) -> Unit
     ) {
-        composable("firstScreen") { FisrtScreen(navController) }
-        composable("secondScreen") { SecondScreen(navController) }
+        NavHost(
+            navController = navController,
+            startDestination = "firstScreen", // Define a string route
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            composable("firstScreen") {
+                setTitle("firstScreen")
+                FisrtScreen(navController)
+            }
+
+            composable("secondScreen") {
+                setTitle("secondScreen")
+                SecondScreen(navController)
+            }
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun DynamicTopAppBar(title: String, navController: NavHostController) {
+        TopAppBar(
+            title = { Text(text = title) },
+            navigationIcon = {
+                if (navController.previousBackStackEntry != null) { // Show back button only if needed
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                }
+            },
+            actions = {
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Filled.MoreVert,
+                        contentDescription = "More Options"
+                    )
+                }
+            }
+        )
     }
 }
 
